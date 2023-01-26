cfb0730a35df25c4cca0604d14ce8c9be34f8e42
Patrick Aziken <onaziken@gmail.com>
2023-01-26 03:34:50 +0100

    Update: Terraform script - Added Load balancer - Added Load balancer target group - Added target listener - Added target listner rule - Added Route Hosted Zone

--- 013_terraform_aws/server_setup.tf
+++ 013_terraform_aws/server_setup.tf
@@ -7,6 +7,13 @@ terraform {
   }
 }
 
+# Variables
+variable "domain_name" {
+  default     = "patrickaziken.me"
+  type        = string
+  description = "Terraform sub-domain"
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
@@ -199,5 +206,98 @@ resource "aws_instance" "altschool-ex13-server3" {
   tags = {
     Name   = "altschool-ex13-server3"
     source = "terraform"
+  }
+}
+
+# Create Load balancer
+resource "aws_lb" "altschool-ex13-lb" {
+  name                       = "altschool-ex13-lb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.altschool-ex13-loadbalancersg.id]
+  subnets                    = [aws_subnet.altschool-ex13-subneta.id, aws_subnet.altschool-ex13-subnetb.id, aws_subnet.altschool-ex13-subnetc.id]
+  enable_deletion_protection = false
+  depends_on                 = [aws_instance.altschool-ex13-server1, aws_instance.altschool-ex13-server2, aws_instance.altschool-ex13-server3]
+}
+
+# Create ALB target group
+resource "aws_lb_target_group" "altschool-ex13-lb-tg" {
+  name        = "altschool-ex13-lb-tg"
+  target_type = "instance"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.altschool-ex13-vpc.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+# Create target listener
+resource "aws_lb_listener" "altschool-ex13-listener" {
+  load_balancer_arn = aws_lb.altschool-ex13-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.altschool-ex13-lb-tg.arn
+  }
+}
+# Create target listener rule
+resource "aws_lb_listener_rule" "altschool-ex13-listener-rule" {
+  listener_arn = aws_lb_listener.altschool-ex13-listener.arn
+  priority     = 1
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.altschool-ex13-lb-tg.arn
+  }
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+}
+
+# Attach LB target group
+resource "aws_lb_target_group_attachment" "altschool-ex13-att1" {
+  target_group_arn = aws_lb_target_group.altschool-ex13-lb-tg.arn
+  target_id        = aws_instance.altschool-ex13-server1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "Altschool-target-group-attachment2" {
+  target_group_arn = aws_lb_target_group.altschool-ex13-lb-tg.arn
+  target_id        = aws_instance.altschool-ex13-server2.id
+  port             = 80
+}
+resource "aws_lb_target_group_attachment" "Altschool-target-group-attachment3" {
+  target_group_arn = aws_lb_target_group.altschool-ex13-lb-tg.arn
+  target_id        = aws_instance.altschool-ex13-server3.id
+  port             = 80
+
+}
+
+# Create hosted zone - Route53
+resource "aws_route53_zone" "terraform-hosted-zone" {
+  name = var.domain_name
+  tags = {
+    Environment = "prod"
+  }
+}
+
+# Create terraform record
+resource "aws_route53_record" "terraform_domain" {
+  zone_id = aws_route53_zone.terraform-hosted-zone.id
+  name    = "terraform.${var.domain_name}"
+  type    = "A"
+  alias {
+    name                   = aws_lb.altschool-ex13-lb.id
+    zone_id                = aws_lb.altschool-ex13-lb.zone_id
+    evaluate_target_health = true
   }
 }
